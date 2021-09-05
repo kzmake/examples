ab0a8f081fc63d8beb8e37826371c6b3c4f60330
kzmake <kzmake.i3a@gmail.com>
2021-09-05 12:16:55 +0900

    basic-service-invocation-py: init app

---
+++ basic-service-invocation-py/microservices/api/gateway/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	ginlogger "github.com/gin-contrib/logger"
+	"github.com/gin-gonic/gin"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/xerrors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+
+	echopb "github.com/kzmake/examples/basic-service-invocation-py/gen/go/echo/v1"
+	greeterpb "github.com/kzmake/examples/basic-service-invocation-py/gen/go/greeter/v1"
+)
+
+type Env struct {
+	Address string `default:"0.0.0.0:8080"`
+	Dapr    struct {
+		Address string `default:"localhost:50001"`
+	}
+}
+
+const prefix = "GATEWAY"
+
+var env Env
+
+func init() {
+	log.Logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout}).With().Timestamp().Logger()
+
+	if err := envconfig.Process(prefix, &env); err != nil {
+		log.Fatal().Msgf("%+v", err)
+	}
+
+	log.Debug().Msgf("%+v", env)
+}
+
+func newGatewayServer(ctx context.Context) (*http.Server, error) {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+
+	r.Use(ginlogger.SetLogger(
+		ginlogger.WithLogger(func(c *gin.Context, _ io.Writer, latency time.Duration) zerolog.Logger {
+			return log.Logger.With().
+				Timestamp().
+				Int("status", c.Writer.Status()).
+				Str("method", c.Request.Method).
+				Str("path", c.Request.URL.Path).
+				Str("ip", c.ClientIP()).
+				Dur("latency", latency).
+				Str("user_agent", c.Request.UserAgent()).
+				Logger()
+		}),
+		ginlogger.WithSkipPath([]string{"/healthz"}),
+	))
+	r.Use(gin.Recovery())
+
+	greeterMux := runtime.NewServeMux(
+		runtime.WithMetadata(func(_ context.Context, _ *http.Request) metadata.MD {
+			return metadata.Pairs("dapr-app-id", "svc-greeter")
+		}),
+	)
+	if err := greeterpb.RegisterGreeterHandlerFromEndpoint(ctx, greeterMux, env.Dapr.Address, []grpc.DialOption{grpc.WithInsecure()}); err != nil {
+		return nil, xerrors.Errorf("Failed to register handler: %w", err)
+	}
+	r.Group("/greeter/v1").Any("/*any", gin.WrapH(greeterMux))
+
+	echoMux := runtime.NewServeMux(
+		runtime.WithMetadata(func(_ context.Context, _ *http.Request) metadata.MD {
+			return metadata.Pairs("dapr-app-id", "svc-echo")
+		}),
+	)
+	if err := echopb.RegisterEchoHandlerFromEndpoint(ctx, echoMux, env.Dapr.Address, []grpc.DialOption{grpc.WithInsecure()}); err != nil {
+		return nil, xerrors.Errorf("Failed to register handler: %w", err)
+	}
+	r.Group("/echo/v1").Any("/*any", gin.WrapH(echoMux))
+
+	return &http.Server{Addr: env.Address, Handler: r}, nil
+}
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	gatewayS, err := newGatewayServer(ctx)
+	if err != nil {
+		log.Fatal().Msgf("Failed to build gateway server: %v", err)
+	}
+	g.Go(func() error { return gatewayS.ListenAndServe() })
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(quit)
+
+	select {
+	case <-quit:
+		break
+	case <-ctx.Done():
+		break
+	}
+
+	cancel()
+
+	log.Info().Msg("Shutting down server...")
+
+	ctx, timeout := context.WithTimeout(context.Background(), 5*time.Second)
+	defer timeout()
+
+	if err := gatewayS.Shutdown(ctx); err != nil {
+		return xerrors.Errorf("failed to shutdown: %w", err)
+	}
+
+	log.Info().Msgf("Server exiting")
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal().Msgf("Failed to run server: %v", err)
+	}
+}
